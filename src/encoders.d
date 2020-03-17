48c087a3c39dc0124e9da27a78ed9bbbbac12a02
Dmitry Olshansky <dmitry@olshansky.me>
2020-03-18 01:32:06 +0300

    Reach parity with jsm4s in encoding with single binary prop
    Gets job done x2 faster, though encoding is hardly a problem

--- src/encoders.d
+++ src/encoders.d
@@ -2,7 +2,17 @@ module encoders;
 
 import std.range.primitives;
 
-alias Encoder(T) = void delegate(T, ref uint, scope void delegate(uint));
+struct Encoder(T) {
+    uint size;
+    void delegate(T, ref uint, scope void delegate(uint)) encoder;
+
+    void encode(T value, ref uint offset, scope void delegate(uint) sink)
+    {
+        encoder(value, offset, sink);
+    }
+}
+
+
 
 Encoder!string enumEncoder(Range)(Range allValues)
 if (isRandomAccessRange!Range) {
@@ -12,26 +22,32 @@ if (isRandomAccessRange!Range) {
     import std.array : array, assocArray;
     auto unique = sort(allValues).uniq().array();
     uint[string] values = unique.zip(iota(cast(uint)unique.length)).assocArray();
-    return (string value, ref uint offset, scope void delegate(uint) sink) {
-        sink(offset + values[value]);
-        offset += values.length;
-    };
+    return Encoder!string(cast(uint)values.length,
+        (string value, ref uint offset, scope void delegate(uint) sink) {
+            sink(offset + values[value]);
+            offset += values.length;
+        }
+    );
 }
 
 Encoder!Row rangeEncoder(Row, Encoders)(Encoders encoders) 
 if (isForwardRange!Row && isForwardRange!Encoders) {
     import std.range : zip;
-    return (Row values, ref uint offset, scope void delegate(uint) sink) {
-        foreach(value, enc;  zip(values, encoders)) {
-            enc(value, offset, sink);
+    import std.algorithm : map, sum;
+    auto size = encoders.map!(enc => enc.size).sum;
+    return Encoder!Row(cast(uint)size,
+        (Row values, ref uint offset, scope void delegate(uint) sink) {
+            foreach(value, enc;  zip(values, encoders)) {
+                enc.encode(value, offset, sink);
+            }
         }
-    };
+    );
 }
 
 
 auto encodeTo(T)(Encoder!T encoder, T data, scope void delegate(uint) sink) {
     uint offset = 0;
-    encoder(data, offset, sink);
+    encoder.encode(data, offset, sink);
 }
 
 auto encodeToArray(T)(Encoder!T encoder, T data) {
--- src/jsm.d
+++ src/jsm.d
@@ -26,28 +26,38 @@ int main(string[] args) {
     }
     switch(args[1]) {
         case "encode":
+            int property;
+            auto result = getopt(args,
+                "p|property", &property
+            );
             if (args.length < 4) {
                 stderr.writefln("xpected:\n\tfaster-jsm encode from.csv to.dat");
                 return 1;
             }
             string from = args[2];
             string to = args[3];
+            stderr.writeln("Using atribute #", property, " as target property");
             auto rawCsv = (cast(char[])readFile(from))
                 .csvReader!string()
                 .map!(r => r.array)
                 .array();
-            auto encoder = rangeEncoder!(string[])(iota(rawCsv[0].length).map!(i =>
-                enumEncoder(iota(rawCsv.length).map!(j => rawCsv[j][i]).array())
-            ));
+            auto encoders = iota(rawCsv[0].length)
+                .map!(i =>
+                    enumEncoder(iota(rawCsv.length).map!(j => rawCsv[j][i]).array())
+                ).array;
+            auto propValues = iota(rawCsv.length).map!(j => rawCsv[j][property]).array().sort.uniq.array;
+            auto attrsEncoder = rangeEncoder!(string[])(encoders[0..property] ~ encoders[property+1..$]);
             auto output = File(to, "w").lockingTextWriter();
-            int cnt;
-            auto store = appender!(uint[])();
+            output.formattedWrite("# attributes: %d properties: B(%s)\n", attrsEncoder.size, propValues.join(","));
+            auto buffer = appender!(uint[])();
             foreach(row; rawCsv) {
-                store.clear();
-                encoder.encodeTo!(string[])(row, (x) { store.put(x); });
-                output.formattedWrite("%(%s %)\n", store.data);
+                buffer.clear();
+                auto attrs = row[0..property] ~ row[property+1..$];
+                attrsEncoder.encodeTo!(string[])(attrs, (x) {
+                    buffer.put(x); 
+                });
+                output.formattedWrite("%(%s %) | %s\n", buffer.data, row[property]);
             }
-            writeln(cnt);
             break;
         default:
             stderr.writefln("Unrecognized sub-command: '%s'", args[1]);
